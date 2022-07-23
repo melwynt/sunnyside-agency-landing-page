bff9e8dd06487b65412596724f526b1fe1d60ae8
Mel <melwynt@gmail.com>
2022-07-23 15:32:27 +0200

    :rocket: Reconfigure project with yarn and vite

---
+++ README.md
@@ -0,0 +1,8 @@
+## Work in progress
+
+### To run project:
+
+- clone repo
+- cd in new folder
+- yarn (to install modules)
+- yarn dev
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <div className="container">
+        <div className="header">
+          <img src="/images/desktop/image-header.jpg" alt="" />
+          <h1>We are creatives</h1>
+        </div>
+        <div className="item item--1"></div>
+        <div className="item item--2"></div>
+        <div className="item item--3"></div>
+        <div className="item item--4"></div>
+        <div className="item item--5"></div>
+        <div className="item item--6"></div>
+        <div className="testimonials"></div>
+        <div className="item item--7"></div>
+        <div className="item item--8"></div>
+        <div className="item item--9"></div>
+        <div className="item item--10"></div>
+        <div className="footer"></div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
